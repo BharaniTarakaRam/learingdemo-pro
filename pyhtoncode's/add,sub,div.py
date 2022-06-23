45a9aa2f46fb8019caae32f06b541f91a99cf9f4
BharaniTarakaRam <105097147+BharaniTarakaRam@users.noreply.github.com>
2022-06-23 15:15:18 +0530

    code is adder or not

---
+++ pyhtoncode's/add,sub,div.py
@@ -0,0 +1,15 @@
+#add,sub,div,mul of two number
+
+num1=int(input("enter number:"))
+num2=int(input("enter number:"))
+add=num1+num2
+sub=num1-num2
+div=num1/num2
+mult=num1*num2
+
+#print(add,sub,div,mult)
+
+print(add)
+print(sub)
+print(div)
+print(mult)
