92bfde48b1f2bca6be5e61da3713afa588c83832
BharaniTarakaRam <105097147+BharaniTarakaRam@users.noreply.github.com>
2022-07-05 12:26:49 +0530

    program

--- pyhtoncode's/randomnumber(game).py
+++ pyhtoncode's/randomnumber(game).py
@@ -4,6 +4,8 @@ guess = None
 while guess != num:
     guess = input("guess a number between 1 and 10: ")
     guess = int(guess)
+    if guess>10:
+        print("the number is greater than ten10")
     if guess == num:
         print("congratulations! you won!")
         break
